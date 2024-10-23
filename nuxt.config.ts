d8aa0659103a41c81c0742b54eaea7561af4161b
yechentide <yechentide@gmail.com>
2024-10-23 22:51:32 +0900

    chore: add Tailwind CSS and PrimeVue dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
+import Aura from "@primevue/themes/aura"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
+  ],
   devtools: { enabled: true },
+  css: [
+    "primeicons/primeicons.css",
+  ],
   compatibilityDate: "2024-04-03",
   typescript: {
     typeCheck: true,
@@ -12,6 +21,14 @@ export default defineNuxtConfig({
         indent: 2,
         quotes: "double",
         semi: false,
+      },
+    },
+  },
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura,
       },
     },
   },
